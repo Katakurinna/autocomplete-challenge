9ef5d97d097b57e84a5e8fa0e7edc41b20f52b95
Katakurinna <alesagustin@gmail.com>
2020-11-11 23:45:11 +0100

    Added testing cases

--- src/test/java/me/cerratolabs/challenge/autocomplete/FileTesting.java
+++ src/test/java/me/cerratolabs/challenge/autocomplete/FileTesting.java
@@ -1,16 +1,17 @@
 package me.cerratolabs.challenge.autocomplete;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertNull;
+@Slf4j
 public class FileTesting {
     private Autocomplete ac;
 
@@ -21,11 +22,11 @@ public class FileTesting {
 
     @Test
     public void testNullPrefix() {
-        assertEquals(null, ac.matches(null));
+        assertNull("Text dont return null",ac.matches(null));
     }
 
     @Test
-    public void test_startWithyo() {
+    public void testStartWithyo() {
         List<String> list = Arrays.asList(
             "you",
             "young",
@@ -33,5 +34,20 @@ public class FileTesting {
             "yourself"
         );
         assertEquals(list, ac.matches("yo"));
+    }
+
+    @Test
+    public void testStartWithyyo() {
+        List<String> list = Collections.emptyList();
+        assertEquals(list, ac.matches("yyo"));
+    }
+
+    @Test
+    public void testStartWithWrite() {
+        List<String> list = Arrays.asList(
+            "write",
+            "writer"
+        );
+        assertEquals(list, ac.matches("write"));
     }
 }
